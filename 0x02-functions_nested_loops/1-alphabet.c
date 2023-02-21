4a68e0f91fc4c86fc64bd632361b972d67ccdd26
megersa <megersadhaf@gmail.com>
2023-02-21 05:44:30 -0800

    commit 2 for task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the english alphabet from a-z.
+ * Return: Nothing.
+ */
+int print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	return (0);
+}
