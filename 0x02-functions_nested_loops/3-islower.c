fab809ef610a14d4ee653c4444f211d1f90a265b
megersa <megersadhaf@gmail.com>
2023-02-21 22:58:25 -0800

    print lower letter

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * main - a program print lower letter
- *
- * return: 1 if lower letter and 0 if not
+ * _islower - a program print lower letter
+ * @c: is an integer parameter
+ * Return: 1 if lower letter and 0 if not
  */
 
 int _islower(int c)
